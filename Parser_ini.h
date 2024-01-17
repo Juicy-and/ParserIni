69d0ef94f0fe83f396bca6ef126c8aaf3878e220
Juicy-and <131388626+Juicy-and@users.noreply.github.com>
2024-01-17 23:05:15 +0300

    Update Parser_ini.h

--- Parser_ini.h
+++ Parser_ini.h
@@ -1,69 +1,81 @@
 #pragma once
 #include <string>
 #include <fstream>
-#include <iostream>
-#include <string>
-#include <typeinfo>
+#include <map>
 
-#define CHAR_SIZE 1024;
 
 class Parser_ini
 {
 private:
-	std::ifstream file;
-	std::string buf[100];
+	std::map<int, std::map<int, std::string>> contIni;
 public:
 	Parser_ini(const char name[])
 	{
+		std::map<int, std::string> tmp;
+		std::string buf, tmpStr;
+		std::ifstream file;
+		int numberOfSection = -1;
+		int numberOfVar = 0;
 		file.open(name);
-		// исключения
+		if (file.is_open())
+		{
+			int j = 0;
+			while (!file.eof())
+			{
+				std::getline(file, buf);
+				if (buf[0] == ';')
+					continue;
+				if (buf.find("Section") != std::string::npos)
+				{
+					if (numberOfSection != -1)
+					{
+						if (contIni.find(numberOfSection) != contIni.end())
+						{
+							for (auto& j : contIni.find(numberOfSection)->second)
+								tmp.insert(j);
+							contIni.erase(contIni.find(numberOfSection));
+						}
+						contIni.insert(std::pair<int, std::map<int, std::string>>(numberOfSection, std::move(tmp)));
+					}
+					auto i = buf.find('n');
+					if (buf[i+2] == ']')
+						numberOfSection = buf[i+1] - '0';
+					j = 0;
+				}
+				if (buf.find("var") != std::string::npos)
+				{
+					auto i = buf.find('r');
+					tmpStr.clear();
+					if (buf[i + 2] == '=')
+						numberOfVar = buf[i + 1] - '0';
+					i = i + 3;
+					while (buf[i] != ';' && i != buf.size())
+						tmpStr += buf[i++];
+					tmp.insert(std::pair<int, std::string>(numberOfVar, tmpStr));
+				}
+			}
+		}
+		if (contIni.find(numberOfSection) != contIni.end())
+		{
+			for (auto& j : contIni.find(numberOfSection)->second)
+				tmp.insert(j);
+			contIni.erase(contIni.find(numberOfSection));
+		}
+		contIni.insert(std::pair<int, std::map<int, std::string>>(numberOfSection, tmp));
+		file.close();
 	}
+
 	template <typename T>  
 	T get_value(const std::string& value)
 	{
-		std::string sect, temp, var;
-		file.seekg(0);
-		auto x = 0;
-		for (; value[x] != '.'; x++)
-			temp += value[x];
-		sect = "[" + temp + "]";
-		x++;
-		for (; value[x] != '\0'; x++)
-			var += value[x];
-		int i = 0;
-		while (!file.eof())
-		{
-			std::getline(file, buf[i]);
-			if (buf[i] == sect)
-			{
-				i++;
-				do
-				{
-					std::getline(file, buf[i]);
-					auto fnd = -1;
-					if (!buf[i].find(var))
-					{
-						fnd = buf[i].find("=");
-						if (fnd != -1)
-						{
-
-							temp.clear();
-							for (int j = fnd + 1; buf[i][j] != '\0'; j++)
-								temp += buf[i][j];
-							T ret;
-							if (typeid(T).name() == typeid(int).name())
-								ret = stoi(temp);
-							else if (typeid(T).name() == typeid(float).name())
-								ret = stof(temp);
-							//else
-								//	ret = temp;
-
-								return ret;
-						}
-					}
-				} while (buf[i][0] != '[');
-			}
-		}
-		return 0;
+			if (typeid(T).name() == typeid(int).name())
+				return stoi(contIni.find(value[value.find('n') + 1] - '0')->second.find(value[value.find('r') + 1] - '0')->second);
+			else if (typeid(T).name() == typeid(float).name())
+				return stof(contIni.find(value[value.find('n') + 1] - '0')->second.find(value[value.find('r') + 1] - '0')->second);
+	}
+	template <>
+	std::string get_value(const std::string& value)
+	{
+		return contIni.find(value[value.find('n') + 1] - '0')->second.find(value[value.find('r') + 1] - '0')->second;
 	}
 };
