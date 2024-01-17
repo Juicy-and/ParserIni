0e543ec33a97ab101699d1fd13ce51ff28c0de33
Juicy-and <131388626+Juicy-and@users.noreply.github.com>
2024-01-17 23:05:28 +0300

    Update Parser_ini.cpp

--- Parser_ini.cpp
+++ Parser_ini.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
 #include "Parser_ini.h"
 
-
+int main()
+{
+	Parser_ini parser("test.txt");
+	std::cout << "Section1.var1 = " << parser.get_value<float>("Section1.var1") << "\n";
+	std::cout << "Section2.var2 = " << parser.get_value<int>("Section2.var2") << "\n";
+	std::cout << "Section1.var3 = " << parser.get_value<std::string>("Section1.var3") << "\n";
+	return 0;
+}
